6484271fccc6e529e502e3f26e16e64888e8d5d6
S <S@mail.minecraft.jp.net>
2015-08-04 00:56:56 +0900

    [REF] Refactor manifest.json

--- js/background.js
+++ js/background.js
@@ -12,3 +12,28 @@ function add_bookmark(info, tab) {
 }
 
 chrome.contextMenus.create({"title": "Add to bookmark", "onclick": add_bookmark});
+
+//Omnibox
+chrome.omnibox.onInputEntered.addListener(
+  function(text) {
+    if(text !== "") {
+      var url = chrome.extension.getURL("/view/omni.html");
+      var createProp = {
+        url: url
+      };
+      chrome.tabs.create(createProp, function(tab) {
+        chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+          if(tabId === tab.id) {
+            chrome.tabs.sendMessage(tab.id, {text: text});
+            text = "";
+          }
+        });
+      });
+    } else {
+      var url = chrome.extension.getURL("/view/popup.html");
+      var createProp = {
+        url: url
+      };
+      chrome.tabs.create(createProp);
+    }
+});
