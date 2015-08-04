faf543c1f675bbc6c60ec01b3dce660382784a97
S <S@mail.minecraft.jp.net>
2015-08-04 10:21:01 +0900

    [FIX] added what to do when InputEntered on omnibox

---
+++ js/omni.js
@@ -0,0 +1,23 @@
+chrome.runtime.onMessage.addListener(function(items, sender, sendRes) {
+  console.log(items);
+  var itemLen = items.length;
+  if(itemLen === 1) {
+    // マッチしたものが1つの場合は転送
+    var url = items[0].url;
+    var updateProp = { url: url };
+    chrome.tabs.update(updateProp)
+  } else {
+    // html生成
+    var html = "";
+    for(var i = 0; i < itemLen; i++) {
+      html += '<div class="col s6"><div class="card blue-grey darken-1"><div class="card-content white-text"><span class="card-title">' + items[i].name + '</span></div>';
+      html += '<div class="card-action"><a href="' + items[i].url + '">開く</a></div></div></div>';
+    }
+    if(itemLen === 0) {
+    // マッチしたものが0つの場合はnothing foundと表示
+      html += '<div class="col s12"><div class="card blue-grey darken-1"><div class="card-content white-text"><span class="card-title">Nothing Found</span></div></div></div>';
+    }
+    // htmlを適応
+    $(document.documentElement).find("#result").append(html);
+  }
+});
